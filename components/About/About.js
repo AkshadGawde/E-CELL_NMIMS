5f35e63d50ca6df83a208e501bd9d686a0efb0e1
aKshad <gawdeakshad@gmail.com>
2024-05-06 01:03:01 +0530

    enhanced Navbar-Hero Section

--- components/About/About.js
+++ components/About/About.js
@@ -10,33 +10,35 @@ const About = () => {
     const [showTimer, setShowTimer] = useState(true)
 
     return (
-        <div className={styles.container} id = 'AboutUS'>
+        <div className={styles.container} id="AboutUS">
             <div className={styles.title_container}>
-                <h1 className={styles.title} >
-                    What is <span>Dhruva&nbsp;?</span>
+                <h1 className={styles.title}>
+                    What is <span>E-CELL &nbsp;?</span>
                 </h1>
 
                 <p className={styles.content}>
-                    Dhruva is the annual tech fest of IIIT Kottayam! It’s the
-                    amalgamation of mind gruelling competitions, thought
-                    provoking expert talks and workshops that stimulate your
-                    cerebral matter!
+                    Welcome to the official website of the Entrepreneurship Cell
+                    (E-Cell) at NMIMS Navi Mumbai. Here, we strive to foster
+                    innovation, entrepreneurship, and leadership among students,
+                    providing a platform for aspiring entrepreneurs to learn,
+                    connect, and grow.
                     <br />
                     <br />
-                    Just like the pole star dhruva, this event is your chance to
-                    shine bright and bring into play the technical talents!
-
+                    Just like the North Star guides sailors, the
+                    Entrepreneurship Cell (E-Cell) at NMIMS Navi Mumbai is your
+                    beacon in innovation and entrepreneurship. Embrace your
+                    technical talents and chart your course to success with us!
                 </p>
                 {/* <div className={styles.btn_container}>
                     <HButtonLight name="About Us" icon = {<ImArrowUpRight2/>}/>
                 </div> */}
             </div>
             <div className={styles.date_container}>
-                <div className={styles.left_date} >
-                    <Date day="28" month="October" subtxt="th" />
+                <div className={styles.left_date}>
+                    <Date day="06" month="May" subtxt="th" />
                 </div>
                 <hr className={styles.line} />
-                <div className={styles.right_date} >
+                <div className={styles.right_date}>
                     <Date day="31" month="October" subtxt="st" />
                 </div>
             </div>
