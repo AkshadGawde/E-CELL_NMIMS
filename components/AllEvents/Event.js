265120eed08685511fec4468f0acce97ce13abec
aKshad <gawdeakshad@gmail.com>
2024-05-16 15:25:08 +0530

    E-Summit Events

--- components/AllEvents/Event.js
+++ components/AllEvents/Event.js
@@ -4,7 +4,7 @@ import AOS from 'aos'
 import 'aos/dist/aos.css'
 import { useEffect } from 'react'
 
-import { events } from '@data/events'
+import { events, happenings } from '@data/events'
 import { getStaticProps } from 'pages/event/[id]'
 export const Event = () => {
     useEffect(() => {
@@ -17,6 +17,22 @@ export const Event = () => {
             <h1 style={{}}>E-Summit 1.0</h1>
             <div className={styles.cards}>
                 {events.map((data) => (
+                    <Card
+                        key={data.id}
+                        id={data.id}
+                        name={data.name}
+                        poster={data.poster}
+                        date={data.date}
+                        time={data.time}
+                        tag={data.tag}
+                        link={data.link}
+                    />
+                ))}
+            </div>
+
+            <h1>Happenings</h1>
+            <div className={styles.cards}>
+                {happenings.map((data) => (
                     <Card
                         key={data.id}
                         id={data.id}
--- data/events.js
+++ data/events.js
@@ -3,8 +3,8 @@ const events = [
         id: 1,
         link: 'Black-Tie',
         name: 'Black-Tie',
-        tag: 'Black-Tie',
-        description: `Experience an evening of elegance and entrepreneurial excellence. Dress in your finest black attire for a night of enriching conversations to celebrate refinement meeting entrepreneurial brilliance.`,
+        tag: 'Elegance and Entrepreneurial Excellence',
+        description: `Experience an evening of elegance and entrepreneurial excellence, crafted by NMIMS Navi Mumbai's E-Cell team. Dress in your finest black attire for a night of enriching conversations, complemented by a sumptuous dinner and motivational keynotes from industry leaders. This event promises a blend of sophistication, interactivity, and outstanding performances. Celebrate refinement meeting entrepreneurial brilliance and create lasting memories in a captivating atmosphere.`,
         date: '14th March 2024',
         time: '7:30pm - 9:30pm',
         organisers: [
@@ -30,9 +30,13 @@ const events = [
         id: 2,
         link: 'Dragons Den',
         name: 'Dragons Den',
-        tag: 'A Great Learning Approach',
-        description:
-            '“Briefly describe the problem your idea addresses. Explain your proposed solution and its unique value proposition.',
+        tag: 'Entrepreneurial Challenge',
+        description: `Join Dragons Den in a team of 2 to 4 and pitch your innovative idea!
+        - Problem Statement: Briefly describe the problem your idea addresses.
+        - Solution Overview: Explain your proposed solution and its unique value proposition.
+        - Market Opportunity: Outline the target market and its potential size.
+        - Implementation Plan: Highlight key steps for bringing your idea to life.
+        - Financial Projections (Optional): Illustrate your projected revenue and cost model.`,
         date: '14th March 2024',
         time: '8:00 am',
         organisers: [
@@ -57,9 +61,13 @@ const events = [
         id: 1,
         link: 'Digital Marketing Competition',
         name: 'Digital Marketing Competition',
-        tag: 'Competitive digital marketing contest',
-        description:
-            'Participants can analyze real-world business challenges faced by startups or established companies related to digital marketing. Identifying target audience and developing buyer personas.',
+        tag: 'Strategic Marketing Challenge',
+        description: `Join the Digital Marketing Competition in a team of 2 to 4 and showcase your strategic marketing skills!
+- Participants will analyze real-world business challenges related to digital marketing.
+- Identify target audience and develop buyer personas.
+- Craft compelling marketing messages across various digital channels.
+- Utilize appropriate social media strategies and content marketing tactics.
+- Implement effective performance tracking and analytics.`,
         date: '15th March 2024',
         time: '6:00pm - 8:00pm',
         organisers: [
@@ -85,10 +93,13 @@ const events = [
         id: 1,
         link: '100 to Million',
         name: '100 to Million',
-        tag: 'Each team must consist of 4-5 members',
-        description:
-            'Carry your laptops for the Presentation round. Judges decisions will be final.',
-        date: '15th March 2024',
+        tag: 'Team Challenge',
+        description: `Join the 100 to Million challenge with a team of 4 to 5 members and embark on a two-day journey towards success!
+- Each team must consist of 4-5 members.
+- Participants must adhere to the given theme and guidelines.
+- Carry your laptops for the Presentation round.
+- Judges' decisions will be final.`,
+        date: '15th March 2023',
         time: '3:30pm - 5:00pm',
         organisers: [
             {
@@ -113,10 +124,11 @@ const events = [
         id: 1,
         link: 'Panel Discussion',
         name: 'Panel Discussion !',
-        tag: 'Platform for learning from accomplished entrepreneurs and industry leaders.',
-        description:
-            'Engaging panel discussions covering vital topics like Scaling Your Startup and Innovation in the Digital Age.',
-        date: '15th March 2024',
+        tag: 'Entrepreneurial Insights',
+        description: `Join the Panel Discussion and gain valuable insights from accomplished entrepreneurs and industry leaders!
+        - Engaging panel discussions covering vital topics like 'Scaling Your Startup' and 'Innovation in the Digital Age.'
+        - Attendees will gain invaluable insights, inspiration, and practical strategies for navigating the entrepreneurial landscape.`,
+        date: '15th March 2023',
         time: '9:00am - 11:00am',
         organisers: [
             {
@@ -135,10 +147,11 @@ const events = [
         id: 1,
         link: 'Squid Game',
         name: 'Squid Game',
-        tag: 'Thrilling event inspired by the popular series, bringing intense challenges of Squid Game into real life.',
-        description:
-            'Adaptation of iconic challenges from the series with strategic gameplay and elimination rounds for suspense.',
-        date: '15th March 2024',
+        tag: 'Thrilling Real-life Challenge',
+        description: `Experience the thrill of 'Squid Game' in real life with our adaptation of iconic challenges from the popular series!
+- Intense challenges inspired by 'Squid Game' featuring strategic gameplay and elimination rounds for suspense.
+- Provides an immersive and unforgettable experience for participants.`,
+        date: '15th March 2023',
         time: '10:00am - 12:00am',
         organisers: [
             {
@@ -163,10 +176,15 @@ const events = [
         id: 1,
         link: 'IdeaHub',
         name: 'IdeaHub',
-        tag: 'An Ultimate Tech Challenge',
-        description:
-            ' Dynamic 2-day tech challenge transcending traditional hackathons. Focusing on fostering innovation and ideation across various domains.',
-        date: '28th October',
+        tag: 'Dynamic Tech Challenge',
+        description: `Join IdeaHub, the ultimate 2-day tech challenge transcending traditional hackathons!
+- Focused on fostering innovation and ideation across various domains.
+- Online presence on UnStop from 11th to 14th March, followed by on-campus ideathon rounds.
+- Opportunities for participants to tackle real-world problems and develop cutting-edge solutions.
+- Engaging activities, workshops, and mentorship sessions.
+- Networking opportunities with industry experts and peers.
+- Exciting prizes and recognition for outstanding ideas and presentations.`,
+        date: '11th - 16th March 2024',
         time: '7:30pm ',
         organisers: [
             {
@@ -180,26 +198,68 @@ const events = [
         registration: '',
         leadLink: '14BEml9QbsCcVn25YWX8kNKz7KK74MvcU2116zx1jt08',
     },
+]
+
+const happenings = [
     {
         id: 1,
-        link: 'Comming Soon',
-        name: 'Comming Soon',
-        tag: 'Comming Soon',
-        description: 'Comming Soon',
-        date: 'TBD',
-        time: 'TBD',
+        link: 'InnovateX',
+        name: 'InnovateX',
+        tag: 'Tag1',
+        description:
+            'Aimed to explore entrepreneurship in India and provide practical guidance. Featuring Faiz Malkani, co-founder of Rippl, to inspire and equip students.',
+        date: '12th Jan 2024',
+        time: '10:00am - 12:00pm',
+        organisers: [
+            {
+                key: '1',
+                name: 'Lorem Ipsum ',
+                email: 'Lorem Ipsum@gmail.com',
+                phoneNumber: 'lorem Ipsum',
+            },
+        ],
+        poster: '',
+        registration: 'https://example.com/registration1',
+        leadLink: 'exampleLeadLink1',
+    },
+    {
+        id: 2,
+        link: 'Shark-Tank',
+        name: 'Shark Tank Event',
+        tag: 'Entrepreneurship Challenge',
+        description: `Join us for an exciting and dynamic Shark Tank event organized by the E-Cell of STME, designed to foster innovation and entrepreneurial spirit among students. This hybrid event, conducted both offline and online, offers participants the flexibility and convenience to showcase their ideas effectively.
+    
+    Event Structure:
+    - Three Rounds of Competition:
+      - Round 1: Quiz - Test your knowledge on entrepreneurship and business fundamentals.
+      - Round 2: Pitching - The top teams will select a product to pitch, while the remaining teams choose from the available options. This round is judged on presentation skills, team participation, and the viability of the proposed solution.
+      - Round 3: Advertising - Create an advertising reel for your product, which will be posted for public voting.
+    
+    - Judging and Prizes:
+      - The pitching round will be judged by esteemed faculty member, Dr. Toral Shah.
+      - Advertising reels will be evaluated based on public votes, adding an exciting interactive element to the competition.
+    
+    Event Timeline:
+    - Day 1 (Offline): Engage in the initial quiz and pitching rounds on campus, allowing for direct interaction and real-time feedback.
+    - Day 2 (Online): Participate in the advertising round from the comfort of your home, culminating in the announcement of winners based on public votes and judges’ evaluations.
+    
+    This two-day event promises to be an enriching experience, providing a platform for students to collaborate, innovate, and hone their entrepreneurial skills. Whether you're pitching groundbreaking ideas or crafting compelling advertisements, this event is an excellent opportunity to learn and grow alongside your peers.
+    
+    Don't miss out on this chance to be part of a transformative journey, guided by industry experts and fueled by the collective energy of aspiring entrepreneurs. Join us at the Shark Tank event and take the first step towards turning your business dreams into reality.`,
+        date: '12th Jan 2024',
+        time: '10:00am - 12:00pm',
         organisers: [
             {
                 key: '1',
                 name: 'Lorem Ipsum',
-                email: 'Lorem Ipsum@gmail.com',
-                phoneNumber: 'Lorem Ipsum',
+                email: 'LoremIpsum@gmail.com',
+                phoneNumber: '123-456-7890',
             },
         ],
         poster: '',
-        registration: 'https://dare2compete.com/o/9dTxDwP?lb=MlXGSqt',
-        leadLink: '1OzpHafZbyBR4HhL0AFzZCxE9wLFvQt5KVvifp2PrxIE',
+        registration: 'https://example.com/registration1',
+        leadLink: 'exampleLeadLink1',
     },
 ]
 
-export { events }
+export { events, happenings }
